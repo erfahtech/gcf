7f01910f514c57a1e4a4e0d1fcb6046a5300b946
erditona <erditonaushaadam@gmail.com>
2023-11-01 14:29:25 +0700

    update insert device

--- insert-device/function.go
+++ insert-device/function.go
@@ -5,14 +5,14 @@ import (
 	"net/http"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
-	beurse "github.com/erfahtech/be_erfahtech"
+	beurse "github.com/erfahtech/be_erfahtech/module"
 )
 
 func init() {
-	functions.HTTP("UrseInsertDevice", ursePost)
+	functions.HTTP("urse-insertdevice", UrseInsertDevice)
 }
 
-func ursePost(w http.ResponseWriter, r *http.Request) {
+func UrseInsertDevice(w http.ResponseWriter, r *http.Request) {
 	// Set CORS headers for the preflight request
 	if r.Method == http.MethodOptions {
 		w.Header().Set("Access-Control-Allow-Origin", "https://erfahtech.github.io")
@@ -24,6 +24,6 @@ func ursePost(w http.ResponseWriter, r *http.Request) {
 	}
 	// Set CORS headers for the main request.
 	w.Header().Set("Access-Control-Allow-Origin", "https://erfahtech.github.io")
-	fmt.Fprintf(w, beurse.InsertDevice(r))
+	fmt.Fprintf(w, beurse.GCFInsertDevice("PASETOPUBLICKEY", "MONGOSTRING", "db_urse",r))
 
 }
