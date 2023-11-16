d1dcea6b5083f8cadf5bc55c797de0e1aa1efafd
erditona <erditonaushaadam@gmail.com>
2023-11-16 13:42:53 +0700

    Add UrseDeleteDevice function to delete device and set CORS headers

---
+++ delete-device/function.go
@@ -0,0 +1,29 @@
+package gcf
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	beurse "github.com/erfahtech/be_erfahtech/module"
+)
+
+func init() {
+	functions.HTTP("urse-deletedevice", UrseDeleteDevice)
+}
+
+func UrseDeleteDevice(w http.ResponseWriter, r *http.Request) {
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "https://erfahtech.github.io")
+		w.Header().Set("Access-Control-Allow-Methods", "DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// Set CORS headers for the main request.
+	w.Header().Set("Access-Control-Allow-Origin", "https://erfahtech.github.io")
+	fmt.Fprintf(w, beurse.GCFHandlerDeleteDevice("PASETOPUBLICKEY", "MONGOSTRING", "db_urse", "devices", r))
+
+}
